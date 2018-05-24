bec395f2620b0b8910d0e7c345ad22e0f9bb7b67
patiphanz <patiphan.sr@ku.th>
2018-05-24 23:23:16 +0700

    Fixed some bugs

--- app/src/main/java/com/example/shoot/manga_reader/MainActivity.kt
+++ app/src/main/java/com/example/shoot/manga_reader/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.shoot.manga_reader
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goTo(view : View) {
+        var temp = ""
+        when(view) {
+            imageView1 -> {
+                temp = "http://www.oremanga.com/forum.php?gid=64"
+            }
+            imageView2 -> {
+                temp = "http://mangakakalot.com"
+            }
+        }
+        var intent : Intent
+        intent = Intent (Intent.ACTION_VIEW, Uri.parse(temp))
+        startActivity(intent)
     }
 }
